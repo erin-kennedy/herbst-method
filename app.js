1324a42b54fa89c98c1dce11c61521da7ede1319
erin-kennedy <59750944+erin-kennedy@users.noreply.github.com>
2023-09-05 17:47:01 -0700

    Update app.js
    Draft 2 - add circle loop

--- app.js
+++ app.js
@@ -12,16 +12,33 @@ gsap.to('.background-section', {
   }
 })
 
-const tl = gsap.timeline({
-	scrollTrigger: {
+// const tl = gsap.timeline({
+// 	scrollTrigger: {
+//         trigger: ".m-section-2",
+//         start: "top top",
+//         end: "bottom top",
+//         toggleActions: "restart none none none",
+//         pin: true,
+//         scrub: 1
+//     }
+// });
+
+// tl.from(".circle-container-1", {x: -200, duration: 2})
+// .from(".circle-container-2", {x: -400, duration: 2})
+
+let circles = gsap.utils.toArray(".circle-container-1");
+
+gsap.from(".circle", {
+    x: function(index, circle, circles) {
+        return (index + 1) * -200;
+    },
+    duration: 2,
+    scrollTrigger: {
         trigger: ".m-section-2",
         start: "top top",
-        end: "bottom top",
+        // end: "bottom top", 
         toggleActions: "restart none none none",
-        pin: true,
-        scrub: 1
+        pin: true
     }
-});
+})
 
-tl.from(".circle-container-1", {x: -200, duration: 2})
-.from(".circle-container-2", {x: -400, duration: 2})
